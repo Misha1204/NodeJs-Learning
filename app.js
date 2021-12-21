306a18125876625f6db7e65877bfbeafa8b4dc32
misha1204 <Mikheil.kvashali@gmail.com>
2021-12-21 11:59:02 +0400

    Get data from request. Chunks and Buffers

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   const url = req.url;
+  const method = req.method;
 
   if (url === "/") {
     res.setHeader("Content-Type", "text/html");
@@ -11,6 +13,23 @@ const server = http.createServer((req, res) => {
       "<body><form action='/message' method='POST'><input type='Text' name='message'><button type='submit'>Send</button></form></body>"
     );
     res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+    });
+
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
     return res.end();
   }
 
