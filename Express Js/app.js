f6ccf8c47192c06bb5bbb01cc6646331f388ea86
misha1204 <Mikheil.kvashali@gmail.com>
2022-01-03 18:38:37 +0400

    add routing, html pages and styles also added util folder for helper functions

--- Express Js/app.js
+++ Express Js/app.js
@@ -1,13 +1,22 @@
+const path = require("path");
+
 const express = require("express");
+
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use("/add-product", (req, res, next) => {
-  res.send("<h1>The Add Product Page</h1>");
-});
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-app.use("/", (req, res, next) => {
-  res.send("<h1>Hello from Express!</h1>");
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/admin", adminRoutes);
+
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000);
