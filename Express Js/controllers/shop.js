413e26d9d83def0acf5614bd195f7cff884cd1d9
Misha1204 <76579747+Misha1204@users.noreply.github.com>
2022-01-13 17:40:44 +0400

    Add new views adn new routes

--- Express Js/controllers/shop.js
+++ Express Js/controllers/shop.js
@@ -10,6 +10,13 @@ exports.getProducts = (req, res, next) => {
   });
 };
 
+exports.getProduct = (req, res) => {
+  const prodId = req.params.productId;
+  Product.findById(prodId, product => {
+    res.render("shop/product-detail", { product, pageTitle: product.title, path: "/products" })
+  });
+}
+
 exports.getIndex = (req, res) => {
   Product.fetchAll((products) => {
     res.render("shop/index", {
